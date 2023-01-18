d63081feb9a949f101fd16720d8544f5928dd2d5
ehsan2105 <105609659+ehsan2105@users.noreply.github.com>
2023-01-18 13:12:01 +0330

    Add files via upload

---
+++ project for hash 256.py
@@ -0,0 +1,11 @@
+from csv import reader
+from hashlib import sha256
+hash_pas= {}
+for pas in range(1,10000):
+    hashing_numbers = sha256(b'%i'% pas).hexdigest()
+    hash_pas[hashing_numbers] = pas
+with open(r"c:\Users\Ehsan\Desktop\2.csv") as f:
+    pas_singer = reader(f)
+    for row in pas_singer:
+        for key in row[1:]:
+            print(row,':',hash_pas[key])
